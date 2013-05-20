287dddae1b6b3d2b2b08b9aa348d85f9c5c0931c
Pier <piercosta91@gmail.com>
2013-05-20 17:52:22 +0200

    piccoli progressi

--- AntiTheft/src/it/unibo/antitheft/MainActivity.java
+++ AntiTheft/src/it/unibo/antitheft/MainActivity.java
@@ -2,7 +2,9 @@ package it.unibo.antitheft;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void openTrackingActivity(View view) {
+		Intent intent = new Intent(this, TrackingActivity.class);
+	    // Do something in response to button
 	}
 
 }
