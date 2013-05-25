eb7301590e12be0c0b5325b7fdf377be37fcedb6
Pier <piercosta91@gmail.com>
2013-05-25 16:22:14 +0200

    modifiche

--- AntiTheft/src/it/unibo/antitheft/TrackingActivity.java
+++ AntiTheft/src/it/unibo/antitheft/TrackingActivity.java
@@ -1,17 +1,22 @@
 package it.unibo.antitheft;
 
-import com.google.android.gms.location.LocationClient;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.support.v4.app.NavUtils;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class TrackingActivity extends Activity {
+public class TrackingActivity extends FragmentActivity {
+
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
 	@SuppressLint("NewApi")
 	@Override
@@ -20,18 +25,102 @@ public class TrackingActivity extends Activity {
 		setContentView(R.layout.activity_tracking);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		
-		//LocationClient mLocationClient = new LocationClient(this, this, this);
-		
-		// Create the text view
-	    TextView textView = new TextView(this);
-	    textView.setTextSize(40);
-	    
-	    textView.setText("prova");
 
-	    // Set the text view as the activity layout
-	    setContentView(textView);
+		//LocationClient mLocationClient = new LocationClient(this, this, this);
+
+		// Create the text view
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+
+		textView.setText("prova");
+
+		// Set the text view as the activity layout
+		setContentView(textView);
 	}
+
+	@SuppressLint("NewApi")
+	public static class ErrorDialogFragment extends DialogFragment {
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+		// Default constructor. Sets the dialog field to null
+		@SuppressLint("NewApi")
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+		// Set the dialog to display
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+		// Return a Dialog to the DialogFragment.
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	}
+
+
+	/*
+	 * Handle results returned to the FragmentActivity
+	 * by Google Play services
+	 */
+	@Override
+	protected void onActivityResult(
+			int requestCode, int resultCode, Intent data) {
+		// Decide what to do based on the original request code
+		switch (requestCode) {
+		case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+			/*
+			 * If the result code is Activity.RESULT_OK, try
+			 * to connect again
+			 */
+			switch (resultCode) {
+			case Activity.RESULT_OK :
+				/*
+				 * Try the request again
+				 */
+				break;
+			}
+		}
+	}
+
+	private boolean servicesConnected() {
+		// Check that Google Play services is available
+		int resultCode =
+				GooglePlayServicesUtil.
+				isGooglePlayServicesAvailable(this);
+		// If Google Play services is available
+		if (ConnectionResult.SUCCESS == resultCode) {
+			// In debug mode, log the status
+			Log.d("Location Updates",
+					"Google Play services is available.");
+			// Continue
+			return true;
+			// Google Play services was not available for some reason
+		} else {
+			// Get the error code
+			int errorCode = connectionResult.getErrorCode();
+			// Get the error dialog from Google Play services
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+					errorCode,
+					this,
+					CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			// If Google Play services can provide an error dialog
+			if (errorDialog != null) {
+				// Create a new DialogFragment for the error dialog
+				ErrorDialogFragment errorFragment =
+						new ErrorDialogFragment();
+				// Set the dialog in the DialogFragment
+				errorFragment.setDialog(errorDialog);
+				// Show the error dialog in the DialogFragment
+				errorFragment.show(getFragmentManager(), "Location Update");
+			}
+		}
+	}
+
+
+
+
 
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
