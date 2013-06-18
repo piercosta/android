ee86a135d0badb6bedae568312eaa69a82eb72a4
Pier <piercosta91@gmail.com>
2013-06-18 14:39:44 +0200

    file generati

--- AntiTheft/gen/it/unibo/antitheft/R.java
+++ AntiTheft/gen/it/unibo/antitheft/R.java
@@ -44,48 +44,49 @@ public final class R {
         public static final int tracking=0x7f070002;
     }
     public static final class string {
-        public static final int IO_Exception_getFromLocation=0x7f050010;
+        public static final int IO_Exception_getFromLocation=0x7f050011;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btn_launch_alarm_activity=0x7f050006;
         /**  Messages 
          */
-        public static final int connected=0x7f050007;
-        public static final int connection_error_code_unknown=0x7f050008;
-        public static final int connection_failed=0x7f050009;
-        public static final int disconnected=0x7f05000a;
-        public static final int get_address=0x7f05000b;
-        public static final int get_location=0x7f05000c;
+        public static final int connected=0x7f050008;
+        public static final int connection_error_code_unknown=0x7f050009;
+        public static final int connection_failed=0x7f05000a;
+        public static final int disconnected=0x7f05000b;
+        public static final int get_address=0x7f05000c;
+        public static final int get_location=0x7f05000d;
         public static final int gps_button=0x7f050002;
         public static final int gps_send_location=0x7f050005;
         public static final int hello_world=0x7f050004;
-        public static final int illegal_argument_exception=0x7f05000d;
-        public static final int invalid_action=0x7f05000e;
-        public static final int invalid_integer_id=0x7f05000f;
+        public static final int illegal_argument_exception=0x7f05000e;
+        public static final int invalid_action=0x7f05000f;
+        public static final int invalid_integer_id=0x7f050010;
         /**  Latitude, longitude format 
          */
-        public static final int latitude_longitude=0x7f050006;
-        public static final int location_requested=0x7f050011;
-        public static final int location_update_error=0x7f050013;
-        public static final int location_update_error_detail=0x7f050014;
-        public static final int location_update_missing_update=0x7f050015;
-        public static final int location_updated=0x7f050012;
-        public static final int location_updates_stopped=0x7f050016;
-        public static final int log_new_location=0x7f050017;
-        public static final int no_address_found=0x7f050018;
-        public static final int no_connection=0x7f050019;
-        public static final int no_geocoder_available=0x7f05001a;
-        public static final int no_location_available=0x7f05001b;
-        public static final int no_resolution=0x7f05001d;
-        public static final int periodic_update_stop=0x7f05001e;
-        public static final int play_services_available=0x7f05001f;
-        public static final int request_location_updates_in_progress=0x7f050020;
-        public static final int resolved=0x7f05001c;
-        public static final int start_updates=0x7f050021;
-        public static final int stop_updates=0x7f050022;
-        public static final int title_activity_alarm=0x7f050025;
+        public static final int latitude_longitude=0x7f050007;
+        public static final int location_requested=0x7f050012;
+        public static final int location_update_error=0x7f050014;
+        public static final int location_update_error_detail=0x7f050015;
+        public static final int location_update_missing_update=0x7f050016;
+        public static final int location_updated=0x7f050013;
+        public static final int location_updates_stopped=0x7f050017;
+        public static final int log_new_location=0x7f050018;
+        public static final int no_address_found=0x7f050019;
+        public static final int no_connection=0x7f05001a;
+        public static final int no_geocoder_available=0x7f05001b;
+        public static final int no_location_available=0x7f05001c;
+        public static final int no_resolution=0x7f05001e;
+        public static final int periodic_update_stop=0x7f05001f;
+        public static final int play_services_available=0x7f050020;
+        public static final int request_location_updates_in_progress=0x7f050021;
+        public static final int resolved=0x7f05001d;
+        public static final int start_updates=0x7f050022;
+        public static final int stop_updates=0x7f050023;
+        public static final int title_activity_alarm=0x7f050026;
         public static final int title_activity_tracking=0x7f050003;
-        public static final int unknown=0x7f050023;
-        public static final int unknown_activity_request_code=0x7f050024;
+        public static final int unknown=0x7f050024;
+        public static final int unknown_activity_request_code=0x7f050025;
     }
     public static final class style {
         /** 
